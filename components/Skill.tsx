044064a0f53a7f4b6b7b5c60f3a96dd3e0c85ff0
Doryan L <dlievre.dev@gmail.com>
2023-10-29 14:21:05 +0100

    chore : change skill img size

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -21,11 +21,11 @@ export default function Skill({
                 transition={{ duration: 0.5 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 src={skill && urlForImage(skill.image).url()}
-                className="rounded-full border border-gray-500 w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+                className="rounded-full border border-gray-500 w-14 h-14 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
             />
-            <div className="absolute opacity-0 group-hover:opacity-80 trnasition duration-500 ease-in-out group-hover:bg-white w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+            <div className="absolute opacity-0 group-hover:opacity-80 trnasition duration-500 ease-in-out group-hover:bg-white w-14 h-14 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
                 <div className="flex items-center justify center h-full">
-                    <p className="text-center text-2xl pb-2 xl:pb-0 xl:pl-1 font-bold text-black opacity-100">
+                    <p className="text-center text-sm md:text-2xl pb-2 xl:pb-0 xl:pl-1 font-bold text-black opacity-100">
                         {skill && skill.numberOfProjectsDone} projets
                     </p>
                 </div>
